0fd3cce25e3bb90b7b89dc4abdff18d6d5a4bb89
Aditya <rajaditya079@gmail.com>
2021-02-26 21:54:55 +0530

    This is practise

---
+++ src/main/java/practice/PracticeClass.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class PracticeClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is just for practise");
+
+	}
+
+}
